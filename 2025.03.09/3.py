8cf04f276881c919e210909f08d3ffdb1d705d55
Gennadiy <renardes@gmail.com>
2025-04-06 09:48:51 +0500

    2025.03.09: оформление 2

--- 2025.03.09/3.py
+++ 2025.03.09/3.py
@@ -1,12 +1,13 @@
 vremia = input('Введите целое число минут затраченных на изготовление изделия ')
 
 vremia_v_chasah = float(vremia) / 60
-vremia_v_celih_chasah = int (vremia_v_chasah)
-minut = float((vremia_v_chasah) - float(vremia_v_celih_chasah))* 60
+vremia_v_celih_chasah = int(vremia_v_chasah)
+minut = float((vremia_v_chasah) - float(vremia_v_celih_chasah)) * 60
 
-print (f'Деталь изготовлена за {vremia} минут.\nВ отчете будет указано время   {vremia_v_celih_chasah} часа и  {int(minut)} минут')
+print(f'Деталь изготовлена за {vremia} минут.\nВ отчете будет указано время   {vremia_v_celih_chasah} часа и  {int(minut)} минут')
 
-#Введите целое число минут затраченных на изготовление изделия 156
-#Деталь изготовлена за 156 минут,
-#В отчете будет указано время   2 часа и  36 минут
+
+# Введите целое число минут затраченных на изготовление изделия 156
+# Деталь изготовлена за 156 минут,
+# В отчете будет указано время   2 часа и  36 минут
 
--- 2025.03.09/4.py
+++ 2025.03.09/4.py
@@ -1,20 +1,22 @@
 chislo = input('Для выполнения математических операций введите положительное трехзначное число: ')
 
 cifra_1_chisla = int(chislo) // 100
-cifra_2_chisla = int(int(chislo) / 10 - cifra_1_chisla * 10)
-cifra_3_chisla = (int(chislo) - cifra_1_chisla * 100 - cifra_2_chisla * 10)
+cifra_2_chisla = int(int(chislo)/10 - cifra_1_chisla*10)
+cifra_3_chisla = int(chislo) - cifra_1_chisla*100 - cifra_2_chisla*10
 
 summa_cifr_chisla = cifra_1_chisla + cifra_2_chisla + cifra_3_chisla
 proizvedenie_cifr_chisla = cifra_1_chisla * cifra_2_chisla * cifra_3_chisla
 
-print (f'Первая цифра: {cifra_1_chisla}')
-print (f'Вторая цифра: {cifra_2_chisla}')
-print (f'Третья цифра: {cifra_3_chisla}')
-print (f'Сумма цифр числа {chislo} равна {summa_cifr_chisla}.\nПроизведение цифр равно {proizvedenie_cifr_chisla} ')
+print(f'Первая цифра: {cifra_1_chisla}')
+print(f'Вторая цифра: {cifra_2_chisla}')
+print(f'Третья цифра: {cifra_3_chisla}')
+print(f'Сумма цифр числа {chislo} равна {summa_cifr_chisla}.\nПроизведение цифр равно {proizvedenie_cifr_chisla} ')
 
-#Для выполнения математических операций введите положительное трехзначное число: 456
-#Первая цифра: 4
-#Вторая цифра: 5
-#Третья цифра: 6
-#Сумма цифр числа 456 равна 15.
-#Произведение цифр равно 120
+
+# Для выполнения математических операций введите положительное трехзначное число: 456
+# Первая цифра: 4
+# Вторая цифра: 5
+# Третья цифра: 6
+# Сумма цифр числа 456 равна 15.
+# Произведение цифр равно 120
+
