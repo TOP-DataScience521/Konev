94ea33fcd762b1a19754ca126e315d560032b8be
Gennadiy <renardes@gmail.com>
2025-04-09 22:38:00 +0500

    2025.03.17: проверено

--- 2025.03.17/1.py
+++ 2025.03.17/1.py
@@ -1,34 +1,37 @@
-
-
-number_1 = float(input ('Введите первое число: '))
+number_1 = float(input('Введите первое число: '))
 
 if number_1 > 0:
+    # УДАЛИТЬ: избыточная операция
     number_1 = number_1
 else:
     number_1 = 0
-     
-number_2 = float(input ('Введите второе число: '))
+
+number_2 = float(input('Введите второе число: '))
 
 if number_2 > 0:
+    # УДАЛИТЬ: избыточная операция
     number_2 = number_2
 else:
     number_2 = 0
-     
-number_3 = float(input ('Введите третье число: '))
+
+number_3 = float(input('Введите третье число: '))
 
 if number_3 > 0:
+    # УДАЛИТЬ: избыточная операция
     number_3 = number_3
 else:
     number_3 = 0
-     
 
 sum_numbers = number_1 + number_2 + number_3
 
-
 print (f'Сумма чисел = {sum_numbers}' )
-     
+
 
 # Введите первое число: 4
 # Введите второе число: -5
 # Введите третье число: 6.5
 # Сумма чисел = 10.5
+
+
+# ИТОГ: 2/3
+
